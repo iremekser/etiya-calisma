4ecd62bdacc524a757a7d37c10a252667253c918
iremekser <iremekser@gmail.com>
2020-09-09 15:41:24 +0300

    optimization

--- project.py
+++ project.py
@@ -4,30 +4,16 @@ import psycopg2
 
 conn = psycopg2.connect(
     "dbname='etiya' user='dwh_stg' host='192.168.1.45' password='Stg1220'")
-cursor = conn.cursor()
 
-owner = pd.DataFrame()
+tables = {'stg_dce_cust': None,
+          'stg_dce_party': {'lsuffix': 'party_id', 'rsuffix': 'party_id'},
+          'stg_dce_gnl_st': {'lsuffix': 'st_id', 'rsuffix': 'gnl_std_id'},
+          'stg_dce_cust_tp': {'lsuffix': 'cust_tp_id', 'rsuffix': 'cust_tp_id'},
+          'stg_dce_gnl_tp': {'lsuffix': 'party_tp_id', 'rsuffix': 'gnl_st_id'},
+          'stg_dce_gnl_st': {'lsuffix': 'st_id', 'rsuffix': 'gnl_st_id'}}
 
-table_names = ['stg_dce_cust', 'stg_dce_party', 'stg_dce_gnl_st',
-               'stg_dce_cust_tp', 'stg_dce_gnl_tp', 'stg_dce_gnl_st']
-tables = {}
-
-for name in table_names:
-    sql = "select * from {}".format(name)
-    tables[name] = (pd.read_sql_query(sql, conn))
-
-m = pd.concat([owner, tables['stg_dce_cust']])
-
-# m = pd.merge(m, tables['stg_dce_party'], how='left', on='party_id')
-m = m.join(tables['stg_dce_party'], lsuffix='party_id', rsuffix='party_id')
-
-m = m.join(tables['stg_dce_gnl_st'], lsuffix='st_id', rsuffix='gnl_st_id')
-
-m = m.join(tables['stg_dce_cust_tp'],
-           lsuffix='cust_tp_id', rsuffix='cust_tp_id')
-
-m = m.join(tables['stg_dce_gnl_tp'],
-           lsuffix='party_tp_id', rsuffix='gnl_tp_id')
-
-m = m.join(tables['stg_dce_gnl_st'], lsuffix='st_id', rsuffix='gnl_st_id')
-
+m = pd.DataFrame()
+for table_name, suffixs in tables.items():
+    df = pd.read_sql_query("select * from {}".format(table_name), conn)
+    m = df if m.empty else m.join(
+        df, lsuffix=suffixs['lsuffix'], rsuffix=suffixs['rsuffix'])
